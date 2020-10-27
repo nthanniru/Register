6b44db9d8e44781dfbdf7723228373c3f820bf15
nthanniru <naresht.info@gmail.com>
2020-10-27 09:10:35 +0530

    Table creation script

---
+++ DB.sql
@@ -0,0 +1,9 @@
+create table register_user
+(First_name varchar2(250),
+ Last_name varchar2(250),
+ gender varchar2(10),
+ eamil varchar2 (250),d
+ password varchar2(250),
+ pnone_number varchar2(10),
+ country varchar2(100),
+ address varchar2(1000))
