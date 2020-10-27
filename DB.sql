9d469d79345a91420599262c653fc3317d63bb01
nthanniru <62169751+nthanniru@users.noreply.github.com>
2020-10-27 11:28:02 +0530

    Update DB.sql
    Removed the 'd' from table creation.

--- DB.sql
+++ DB.sql
@@ -2,7 +2,7 @@ create table register_user
 (First_name varchar2(250),
  Last_name varchar2(250),
  gender varchar2(10),
- eamil varchar2 (250),d
+ eamil varchar2 (250),
  password varchar2(250),
  pnone_number varchar2(10),
  country varchar2(100),
