bb79106287b72e9f97c70787b610594ed7b2f435
nthanniru2 <nthanniru2@gmail.com>
2021-01-21 22:46:33 +0530

    Added the postgres Changes

--- DB.sql
+++ DB.sql
@@ -1,3 +1,4 @@
+-- Table and sequence creation in Oracle.
 create table register_user
 (First_name varchar2(250),
  Last_name varchar2(250),
@@ -14,3 +15,25 @@ maxvalue 999999999999999999999999999
 start with 1
 increment by 1
 cache 20;
+---------------------------
+--Table Creation in Postgressql.
+----------------------------
+create table register_user
+(First_name VARCHAR(250),
+ Last_name VARCHAR(250),
+ gender VARCHAR(10),
+ eamil VARCHAR (250),
+ password VARCHAR(250),
+ pnone_number VARCHAR(10),
+ country VARCHAR(100),
+ address VARCHAR(1000))
+ ----------------------------------
+-- Adding Sequence to table postgres
+-----------------------------------
+ create sequence s_register_user
+minvalue 1
+maxvalue 9999999
+start with 1
+increment by 1
+cache 20;
+--------------------
--- src/com/nthanniru/dao/DB.java
+++ src/com/nthanniru/dao/DB.java
@@ -1,3 +1,50 @@
+/*package com.nthanniru.dao;
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+public class DB {
+
+
+	
+public static Properties loadPropertiesFile() throws Exception {
+		 
+		Properties prop = new Properties();
+		InputStream in = new FileInputStream("JDBC");
+		prop.load(in);
+		in.close();
+		return prop;
+	}
+
+	public static Connection getCon() {
+		Connection con = null;
+		try {
+			//Class.forName("oracle.jdbc.driver.OracleDriver");
+			//con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+			
+			Properties prop = loadPropertiesFile();
+			 
+			String driverClass = prop.getProperty("Driver_Name");
+			String url = prop.getProperty("DB_URL");
+			String username = prop.getProperty("Username");
+			String password = prop.getProperty("Password");
+	 
+			Class.forName(driverClass);
+	 
+			con = DriverManager.getConnection(url, username, password);
+			
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		return con;
+	}
+ 
+ 
+}*/
 package com.nthanniru.dao;
 
 import java.sql.*;
@@ -7,8 +54,11 @@ public class DB {
 	public static Connection getCon() {
 		Connection con = null;
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+			 Class.forName("org.postgresql.Driver");
+	         con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "postgres");
+	         
+			//Class.forName("oracle.jdbc.driver.OracleDriver");
+			//con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
