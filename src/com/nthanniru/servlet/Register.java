46735fb834e2f2e3324b259edade7105a7b71166
nthanniru <62169751+nthanniru@users.noreply.github.com>
2023-02-20 15:14:21 +0530

    Added Line  4

--- src/com/nthanniru/servlet/Register.java
+++ src/com/nthanniru/servlet/Register.java
@@ -29,6 +29,7 @@ public class Register extends HttpServlet {
 		// Added Line One
 		// Added Line 2
 		Added Line  3
+		Added Line  4
 		String ln = request.getParameter("Last_Name");
 		String g = request.getParameter("Gender");
 		String e = request.getParameter("Email");
