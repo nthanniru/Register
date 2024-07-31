f34454c4b5ba8d47c3211e324ef74778f9599e45
nthanniru <naresht.info@gmail.comu>
2024-07-31 14:32:17 +0530

    Updated the code

--- DB.sql
+++ DB.sql
@@ -1,3 +1,5 @@
+Oracle database script:
+----------------------
 -- Table Creation
 create table register_user
 (First_name varchar2(250),
@@ -8,6 +10,25 @@ create table register_user
  pnone_number varchar2(10),
  country varchar2(100),
  address varchar2(1000))
+-- Adding Sequence to table
+ create sequence register_user
+minvalue 1
+maxvalue 999999999999999999999999999
+start with 1
+increment by 1
+cache 20;
+/
+Postgres database script:
+-------------------------
+create table register_user
+(First_name VARCHAR(250),
+ Last_name VARCHAR(250),
+ gender VARCHAR(10),
+ eamil VARCHAR(250),
+ password VARCHAR(250),
+ pnone_number VARCHAR(10),
+ country VARCHAR(100),
+ address VARCHAR(1000));
 -- Adding Sequence to table
  create sequence register_user
 minvalue 1
--- src/com/nthanniru/dao/DB.java
+++ src/com/nthanniru/dao/DB.java
@@ -7,8 +7,15 @@ public class DB {
 	public static Connection getCon() {
 		Connection con = null;
 		try {
+			/*  If you are using oracle database Load the oracle JDBC driver
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "postgres", "postgres");
+			*/
+			
+			// If you are using PostgreSQL database Load the PostgreSQL JDBC driver
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://192.168.56.14:5432/postgres", "postgres", "postgres");
+
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
